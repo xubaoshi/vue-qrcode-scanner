0f156becf0ce16b80e9d58bd208c15b821adfce2
xubaoshi <xubaoshi@xubaoshi.com>
2016-08-29 22:25:20 +0800

    add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by xu on 2016/8/29.
+ */
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 4000;
+var open = require('open');
+var app = express();
+
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').__express);
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(port, '', function(err){
+	if(err) return;
+	console.log('Server started, listening on port:' + port)
+	open('http://localhost:' + port + '/')
+});
