ad7f59b86f3fdf2782e705b59d531ba42d0f00e2
xubaoshi <948869516@qq.com>
2016-09-05 14:30:05 +0800

    add  js qrcode  test  code

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.engine('html', require('ejs').__express);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res){
-	res.render('index1');
+	res.render('index3');
 });
 
 app.listen(port, '', function(err){
