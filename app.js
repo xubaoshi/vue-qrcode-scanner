56b94ebf817a06fbca2e46f8dfe92cda54618811
xubaoshi <948869516@qq.com>
2016-09-06 11:57:08 +0800

    add  js qrcode  test  code

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
  * Created by xu on 2016/8/29.
  */
 var express = require('express');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 var path = require('path');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 4000;
 var open = require('open');
+
+var options = {
+	key : fs.readFileSync('./cert/privatekey.pem'),
+	cert : fs.readFileSync('./cert/certificate.pem')
+}
 var app = express();
 
 app.use(bodyParser());
@@ -19,8 +27,8 @@ app.get('/', function(req, res){
 	res.render('angular');
 });
 
-app.listen(port, '', function(err){
+server = https.createServer(options, app).listen(port, '', function(err){
 	if(err) return;
 	console.log('Server started, listening on port:' + port)
-	open('http://localhost:' + port + '/')
+	open('https://localhost:' + port + '/')
 });
